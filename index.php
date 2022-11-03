84e58d3aaeb29ca110fdd069fb1d36c7b3dfc71b
David Laignelet <laignelet.david@gmail.com>
2022-11-03 05:54:13 +0100

    Add evenet page and config controller

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php
 
-$content = './vue/accueil.phtml';
-include ('./layout/layout.phtml');
-
-
+if (isset($_GET['page']) && ($_GET['page'] === 'event')) {
+    $content = './vue/event.phtml';
+    include ('./layout/layout.phtml');
+}
+else {
+    $content = './vue/accueil.phtml';
+    include ('./layout/layout.phtml');
+}
 
 
 
